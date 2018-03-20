8dcecdddac3e6c9eac27de4b4b99d211b48ba7d4
David Brokaw <dgbrokaw2@gmail.com>
2018-03-20 16:56:01 -0400

    depth-first traversal

--- src/iterator-depth-first.js
+++ src/iterator-depth-first.js
@@ -1,5 +1,39 @@
+import asArray from "./helpers/as-array.js";
+
 export default class DepthFirstTreeIterator {
-  constructor() {
-    
+  constructor(trees) {
+    this.trees = asArray(trees);
+    this.result = null;
+  }
+
+  isDone() {
+    return Array.isArray(this.result);
+  }
+
+  start() {
+    this.result = [];
+    return this;
+  }
+
+  traverse(process, test) {
+    this.start();
+    var result = this.result;
+
+    var f = function(node) {
+      if (!test || test(node)) {
+        result.push(process(node));
+      }
+      if (node.hasChildren()) {
+        for (var i=0; i<node.children.length; i++) {
+          f(node.children[i]);
+        }
+      }
+    }
+
+    for (var i=0; i<this.trees.length; i++) {
+      f(this.trees[i]);
+    }
+
+    return this.result;
   }
 }
--- test/iterator-test.js
+++ test/iterator-test.js
@@ -1,6 +1,26 @@
 var DepthFirstTreeIterator = require("../dist/index.js").DepthFirstTreeIterator;
+var Node = require("../dist/index.js").Node;
+var Tree = require("../dist/index.js").Tree;
 
 exports["DepthFirstTreeIterator: constructor"] = function(test) {
+  var iterator1 = new DepthFirstTreeIterator();
+  test.ok(Array.isArray(iterator1.trees) && iterator1.trees.length === 0);
+  var iterator2 = new DepthFirstTreeIterator(new Node());
+  test.ok(Array.isArray(iterator2.trees) && iterator2.trees.length === 1);
+  test.done();
+}
+
+exports["DepthFirstTreeIterator: isDone & start"] = function(test) {
   var iterator = new DepthFirstTreeIterator();
+  test.ok(!iterator.isDone());
+  iterator.start();
+  test.ok(iterator.isDone());
+  test.done();
+}
+
+exports["DepthFirstTreeIterator: traverse"] = function(test) {
+  var iterator = new DepthFirstTreeIterator(Tree.parse('O[A[A1,A2],B,C[C1[C11]]]'));
+  iterator.traverse(n => n.value);
+  test.deepEqual(iterator.result, ['O','A','A1','A2','B','C','C1','C11']);
   test.done();
 }
